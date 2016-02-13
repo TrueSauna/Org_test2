96b5e3c3840daf4262392365c20f6725db24a605
Agoneri <tumaline@hotmail.com>
2016-02-13 11:03:07 +0200

    Ball to its own class

--- index.android.js
+++ index.android.js
@@ -16,8 +16,18 @@ var CIRCLE_SIZE = 80;
 var CIRCLE_COLOR = 'blue';
 var CIRCLE_HIGHLIGHT_COLOR = 'green';
 
-
 var Org_test2 = React.createClass({
+  render: function() {
+    return (
+      <View>
+        <Ball></Ball>
+        <Ball></Ball>
+      </View>
+    )
+  }
+});
+
+var Ball = React.createClass({
 
   _panResponder: {},
   _panResponder2: {},
@@ -52,47 +62,16 @@ var Org_test2 = React.createClass({
         top: this._previousTop
       }
     };
-
   },
-
 
   render: function() {
     return (
-
-      <View>
-        <Picker
-          selectedValue={this.state.language}
-          onValueChange={(lang) => this.setState({language: lang})}>
-            <Picker.Item label="Move" value="1" />
-            <Picker.Item label="Ghost" value="2" />
-            <Picker.Item label="Ghost with line" value="3" />
-            <Picker.Item label="No effect" value="4" />
-        </Picker>
-
-
         <View style={styles.container}>
           <View ref={component => this.asd = component}{...this.props}
             style={styles.circle}
             {...this._panResponder.panHandlers}>
           </View>
         </View>
-
-
-        {/* miten tähän omat animaatiohookit, koko hoito kun toimii vaan 1 objektille
-
-          https://facebook.github.io/react-native/docs/panresponder.html
-          http://blog.zmxv.com/2016/01/lets-write-a-mobile-game-with-react-native-part-2.html
-
-          */}
-        <View style={styles.container}>
-          <View ref={component => this.asd = component}{...this.props}
-            style={styles.circle}
-            {...this._panResponder.panHandlers}>
-          </View>
-        </View>
-
-
-      </View>
     );
   },
 
